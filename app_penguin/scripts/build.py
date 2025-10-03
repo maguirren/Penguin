ba8c40fcc296c2b84af42135bf9657dcc5eb81f7
Mafer <marguirre.dev@gmail.com>
2025-10-02 22:33:37 -0500

    Create build function

--- app_penguin/scripts/build.py
+++ app_penguin/scripts/build.py
@@ -1,5 +1,86 @@
+from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+import yaml
+import markdown
+import shutil
+from .get_config import *
+
+# path del sitio
+DEFAULT_SITE = get_site_path()
+# path de los borradores
+DRAFTS_PATH = DEFAULT_SITE / "drafts"
+# path de los posts no servidos(aun editable)
+POSTS_MD_PATH = DEFAULT_SITE / "posts"
+# ruta de las plantillas
+TEMPLATE_PATH = DEFAULT_SITE / ".templates"
+# ruta del blog
+BLOG_PATH = DEFAULT_SITE / "blog"
+# path de los posts a servir
+POSTS_HTML_PATH = DEFAULT_SITE / "blog" / "posts"
+# ruta de imagenes o assets
+STATICS_PATH = DEFAULT_SITE / "blog" / "statics"
+
+
+# Retorna lista con las rutas de los borradores
+def get_md_file():
+    files = []
+    for filename in DRAFTS_PATH.glob("*.md"):
+        filepath = DRAFTS_PATH / filename
+        files.append(filepath)
+    return files
+
+
+# Elimina todos los archivos existentes en el blog
+def remove_existing_file():
+    if BLOG_PATH.exists() and BLOG_PATH.is_dir():
+        for file in BLOG_PATH.rglob("*"):
+            if file.is_file():
+                file.unlink()
+
+
+# Retorna lista de diccionarios con el contenido de cada borrador
+def get_data(filepath):
+    all_data = []
+    for file in filepath:
+        file_path = Path(file)
+        file_content = file_path.read_text(encoding="utf-8")
+
+        if file_content.startswith('---'):
+            parts = file_content.split('---', 2)
+            data = yaml.safe_load(parts[1])
+            content = parts[2].strip() if len(parts) > 2 else ''
+
+            final_result = data | {"content": content}
+            all_data.append(final_result)
+    return all_data
+
+
 def build():
-    pass
+    files = get_md_file()
+    all_data = get_data(files)
+    
+    remove_existing_file()
+
+    env = Environment(loader=FileSystemLoader(TEMPLATE_PATH))
+
+    post_template = env.get_template('single-post.html')
+    for item in all_data:
+        if item['draft'] == False:
+            item['content'] = markdown.markdown(item['content'])
+            rendered_html = post_template.render(item)
+            filename = POSTS_HTML_PATH / f"{item['title'].replace(' ', '-')}.html"
+            filename.write_text(rendered_html)
+        else:
+            pass
 
 
-build()
+    index_template = env.get_template('index.html')
+    index_filename = BLOG_PATH / 'index.html'
+    #rendered_index = index_template.render()
+    rendered_index = index_template.render(posts=[p for p in all_data if not p['draft']])
+    index_filename.write_text(rendered_index)
+
+    about = TEMPLATE_PATH / 'about-me.html'
+    shutil.copy(about, BLOG_PATH)
+
+
